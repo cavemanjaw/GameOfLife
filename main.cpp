80eb5c9aaf31998a576e2c54f3c839ccbd941bdf
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2017-03-11 20:49:37 +0100

    Removed hardcoded variable form simulation steps

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@ int main()
 	sim_matrix simMatrix;
 	int matrixSize;
 	bool isPrettySet = 0;
-
+	int simSteps;
 
 	std::cout << "Type matrix size" << std::endl;
 	std::cin >> matrixSize;
+	
+	std::cout << "How many steps to simulate?" << std::endl;
+	std::cin >> simSteps;
 
 	std::cout << "Type 1 for pretty printing" << std::endl;
 	std::cin >> isPrettySet;
@@ -28,7 +31,7 @@ int main()
 	}
 
 	std::cout << std::endl;
-	for (int i = 0; i < 10; ++i)
+	for (int i = 0; i < simSteps; ++i)
 	{
 		DoSimStep(simMatrix);
 		
