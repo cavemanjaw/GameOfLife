ab618d589f16d9d7b14892d8da5d7eb981b65227
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2017-03-11 21:23:33 +0100

    Started implementation of SImCell class

--- GameOfLife.h
+++ GameOfLife.h
@@ -3,6 +3,25 @@
 
 #include <vector>
 
+//TODO: Encapsulate sim_matrix in a class with methods to get better interface and firlds for statisticd
+
+//TODO: Create entire class of SimCell to obtain statistics and encaplsulate fields for better interface handling
+
+class SimCell
+{
+public:
+	SimCell();
+	void SetAlive();
+	void SetDead();
+protected:
+private:
+	bool isAlive;
+
+	// For really long simulations ;)
+	long long respawnCounter;
+	long long killCounter;
+};
+
 typedef std::vector<std::vector<bool>> sim_matrix;
 
 void PrintSimMatrix(sim_matrix& simMatrix);
